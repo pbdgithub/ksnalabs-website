5ecea92d0e19a90630c00c09b433691d7b6cf05c
Rajasekar T <kt.rajasekar@gmail.com>
2022-12-04 17:28:28 +0530

    Removed Journey Component from App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Header } from "./components/header";
 import { About } from "./components/about";
 import { Services } from "./components/services";
 import { Our_Clients } from "./components/ourclients";
-import { Our_Journey } from "./components/journey";
 import { Technology } from "./components/technology";
 import { Training } from "./components/training";
 import { Careers } from "./components/careers";
@@ -35,7 +34,6 @@ const App = () => {
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       <Our_Clients data={landingPageData.Our_Clients}/>
-      <Our_Journey data={landingPageData.Our_Journey}/>
       <Technology data={landingPageData.Technology}/>
       <Training data={landingPageData.Training}/>
       <Careers data={landingPageData.Careers} />
